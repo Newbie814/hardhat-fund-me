495bbca22def89aa41f9f6baec2f89b508ce986c
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2022-10-18 21:42:26 -0400

    setup testing for funding with multiple funders

--- test/unit/FundMe.test.js
+++ test/unit/FundMe.test.js
@@ -73,5 +73,51 @@ describe("FundMe", async function () {
                 endingDeployerBalance.add(gasCost).toString()
             )
         })
+        it("allows to withdraw with multiple funders", async function () {
+            // arrange
+            const accounts = await ethers.getSigners()
+            for (let i = 1; i < 6; i++) {
+                const fundMeConnectedContract = await fundMe.connect(
+                    accounts[i]
+                )
+                await fundMeConnectedContract.fund({ value: sendValue })
+            }
+
+            const startingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const startingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // act
+            const txResponse = await fundMe.withdraw()
+            const txReceipt = await txResponse.wait(1)
+            const { gasUsed, effectiveGasPrice } = txReceipt
+            const gasCost = gasUsed.mul(effectiveGasPrice)
+
+            const endingFundMeBalance = await fundMe.provider.getBalance(
+                fundMe.address
+            )
+            const endingDeployerBalance = await fundMe.provider.getBalance(
+                deployer
+            )
+
+            // assert
+            assert.equal(endingFundMeBalance, 0)
+            assert.equal(
+                startingFundMeBalance.add(startingDeployerBalance).toString(),
+                endingDeployerBalance.add(gasCost).toString()
+            )
+
+            // make sure the funders are reset
+            await expect(fundMe.funders(0)).to.be.reverted
+            for (i = 1; i < 6; i++) {
+                assert.equal(
+                    await fundMe.addressToAmountFunded(accounts[i].address),
+                    0
+                )
+            }
+        })
     })
 })
